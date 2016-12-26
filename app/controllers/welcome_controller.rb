e1a6d2f53bae00cff6b9d62cf098f3d3acad009d
John Bao Nguyen <waejay@Johns-MacBook-Pro.local>
2016-12-25 22:27:29 -0600

    Main visible sites, sign/login, user authentication working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,14 @@ class WelcomeController < ApplicationController
 
   def howto
   end
+
+  def signup
+  	if user_signed_in?
+  		redirect_to root_path
+  	else
+  		redirect_to new_user_session_path
+  	end
+  	
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 	
+  	devise_for :users, controllers: { registrations: "registrations"}
 	# default page root 'controller#page'
 	root 'welcome#home' 
 
 	# creates the pages
-	get 'home', to: 'welcome#home'
-  get '/howto', to: 'welcome#howto'
+	get '/home', to: 'welcome#home'
+    get '/howto', to: 'welcome#howto'
+
+    devise_scope :user do
+    	get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    	get "/sign_up" => "devise/registrations#new"
+  end
 end
